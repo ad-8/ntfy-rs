922a8711fb957423aefdda168dcb270159fdc88f
Alex D <alex8d@pm.me>
2023-05-02 14:44:11 +0200

    impl api for basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Specify the target and content of a notification.
+pub struct Options {
+    /// The URL of the ntfy server, e.g. `"http://ntfy.sh"`.
+    pub server: String,
+
+    /// A topic which acts like a channel that receives notifications for that topic, e.g. `backup`.
+    pub topic: String,
+
+    /// A uuid (or another password like String) that will be appended to the topic for privacy reasons.
+    pub uuid: String,
+
+    /// The title of the notification. Defaults to `server/topic_uuid` if `None`.
+    pub title: Option<String>,
+
+    /// The body text of the notification.
+    pub message: String,
+
+    /// One tag or multiple tags as a comma separated String, e.g. `important` or "foo,bar"`.
+    ///
+    /// If a tag matches an [emoji short code](https://docs.ntfy.sh/emojis/), it'll be converted to an emoji
+    /// and prepended to title, otherwise it will be listed below the notification.
+    ///
+    /// Will simply be ignored if `None`.
+    pub tags: Option<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Sends a notifications as specified by the options and returns the response of the POST request.
+pub async fn ntfy(opts: Options) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+    let url = format!("{}/{}_{}", opts.server, opts.topic, opts.uuid);
+    let title = match opts.title {
+        Some(s) => s,
+        None => "".into(),
+    };
+    let tags = match opts.tags {
+        Some(s) => s,
+        None => "".into(),
+    };
+    let client = reqwest::Client::new();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let res = client
+        .post(url)
+        .header("Title", title)
+        .header("Tags", tags)
+        .body(opts.message)
+        .send()
+        .await?;
+
+    Ok(res)
 }
